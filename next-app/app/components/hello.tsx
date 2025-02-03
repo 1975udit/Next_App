a886fc8d89743d98a2b4db79c8c2f67e12aeb2a5
1975udit <113414526+1975udit@users.noreply.github.com>
2025-02-03 20:04:12 +0000

    +--

---
+++ next-app/app/components/hello.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react';
+
+const Hello = () => {
+    console.log("It's me, Hello component!");
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+        </div>
+    );
+};
+
+export default Hello;
--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,9 +1,11 @@
+import Hello from "./components/hello";
 
 export default function Home() {
   console.log("Where i am going to be rendered?");
   return (
     <div>
       <h1>Hello World</h1>
+      <Hello />
     </div>
   );
 }
