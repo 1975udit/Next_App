d7c94a06ce26a99780d6da740e66842624942f7e
1975udit <113414526+1975udit@users.noreply.github.com>
2025-02-04 11:11:57 +0000

    data fetching

--- next-app/app/(dashboard)/dashboard/page.tsx
+++ next-app/app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
-const dashboard = () => {
+const dashboard =async () => {
+  const response  = await fetch('https://jsonplaceholder.typicode.com/albums')
+  if(!response.ok){
+    throw new Error('Failed to fetch data')
+  }
+  const albums = await response.json()
   return (
-    <div>dashboard</div>
+    <div className='grid md:grid-cols-4 gap-4 sm:grid-cols-1'>
+      {albums.map((album : {id : number, title : string}) => (
+        <div key={album.id}
+        className='bg-white shadow-md p-4 rounded-lg transition t...'>
+          <h3 className='text-lg text-purple-200 mb-2 font-bold'>{album.title}</h3>
+          <p className='text-gray-600'>Album ID : {album.id}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
